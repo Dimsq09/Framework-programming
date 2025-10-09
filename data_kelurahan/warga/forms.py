56d1c30c3b14e1fc38a29e536416e3e626405332
Dhimaz <kingofluck0707@gmail.com>
2025-10-09 20:08:05 +0700

    d

--- data_kelurahan/warga/forms.py
+++ data_kelurahan/warga/forms.py
@@ -5,4 +5,4 @@ class WargaForm(forms.ModelForm):
     class Meta:
         model = Warga
         # Tentukan field mana saja dari model yang ingin ditampilkan di form
-        fields = ['nik', 'nama_lengkap', 'alamat', 'no_telepon']/
+        fields = ['nik', 'nama_lengkap', 'alamat', 'no_telepon']
